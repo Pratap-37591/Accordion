6ecfc75756104235783f0d56c51aaaac4ca352d0
prataphb <prathaphb1310@gmail.com>
2023-04-14 16:32:26 +0530

    Added Index Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+// import Accordion from "./Components/Accordion";
+// import Dropdown from "./Dropdown";
+// import CustomMenuBar from "./CustomMenuBar";
+import Translate from "./Translate";
 
-function App() {
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "Prakasj told me React is a frontend javascript framework",
+//   },
+//   {
+//     title: "Why is React?",
+//     content: "React is a favorite JS library among students at UPSHOT",
+//   },
+//   {
+//     title: "hOW do you use React?",
+//     content: "any one can use React by creating components",
+//   },
+// ];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+];
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <br />
+      {/* <Dropdown selected={selected} onSelectedChange={setSelected}  options={options}/> */}
+
+      {/* <CustomMenuBar/> */}
+
+      <Translate />
+    </>
   );
-}
+};
 
 export default App;
